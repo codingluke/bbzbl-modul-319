0653105ef5195461c32fab7e607a9c9dfc1b9376
Lukas Hodel <lukas.hodel@swisscom.com>
2023-03-16 21:27:03 +0100

    if with mermaid

--- docs/3a-konditionen/index.md
+++ docs/3a-konditionen/index.md
@@ -13,27 +13,171 @@ Mit `if` und `switch` können Sie auf unterschiedliche Werte in Ihren Variablen 
 
 Mit dem Schlüsselwort **if** _(engl. falls)_ in Kombination mit dem **Datentyp boolean** können Programme erstellt werden, die abhängig von Benutzereingaben oder Variablenwerten unterschiedlich reagieren.
 
-### `if`-Struktur
+### Bedingungen (boolean)
 
-Eine Struktur mit einem if sieht so aus, wie im nächsten Auszug dargestellt. 
+Bedingungen sind Code-Ausdrücke, welche entweder wahr `true` oder falsch `false` sein können. Diese werden mit dem Datentyp `boolean` ausgedrückt. Diese sind elementar für die `if`-Kontrollstruktur um zu entscheiden, welcher Codeblock ausgeführt werden soll.
 
-- Die Anweisungen sind eine Folge von Programm-Befehlen. 
-- Nur der erste Block ist zwingend, und zwar von `if` bis zur ersten schliessenden geschweiften Klammer. 
-- Die beiden nächsten in türkis (`else-if`) und grau (`else`) unterlegten Blöcke können auch noch mehrfach wiederholt folgen, aber können nie alleine stehen. 
-- Das türkis unterlegte, <u>optionale</u> `else if` dient dazu, in einem if-Ausdruck noch weitere Prüfungen vorzunehmen. 
-- Grau unterlegte ist der <u>optionale</u> `else`-Abschnitt, der in allen zu anderen Auswertungen passenden fällen ausgeführt wird.
+```java title="Beispiel Bedingungen"
+// mit int
+int age = 21;                // gegeben ist eine int Variable
+boolean is21    = age == 21; // true
+boolean isNot21 = age != 21; // false, oder !is21
+boolean isAdult = age >= 18; // true
+boolean isChild = age <= 18; // false
 
-```java
-if (/* Bedingung1 */) { 
-    /* Code-Anweisung welche ausgeführt wird, 
-       falls die Bedingung1 wahr, also true ist. */
-} else if (/* Bedingung2 */) { 
-    /* Code-Anweisung welche ausgeführt wird, falls die 
-       Bedingung1 falsch, die Bedingung2 aber wahr ist */
-} else { // Optionaler block 
-    /* Code-Anweisung welche ausgeführt wird, falls die 
-       Bedingung1 sowie auch die Bedingung2 falsch sind */
+// mit String
+String type = "auto";                 // gegeben ist eine String Variable
+boolean isAuto = "auto".equals(type); // true
+```
+
+:::info
+**String** Variablen **immer mit `.equals()`** vergleichen, <u>nie</u> mit `==`
+:::
+
+### `if`-Anatomie
+
+Eine Kontrollstruktur mit einem `if` sieht so aus, wie im nächsten **Theoretischen Beispiel** dargestellt.
+
+Es folgt ein **Praktisches Beispiel** um es zu veranschaulichen.
+
+### Formelles Beispiel
+
+:::note
+
+Die Bezeichnungen `<Bedingung>` und `<Anweisung>` dienen als <u>Platzhalter</u> rsp. Pseudocode und sind <u>nicht korrekter Java-Code</u>. Im praktischen Beispiel werden diese mit korrektem Java-Code ersetzt.
+
+:::
+
+<div className="theme-code-block-highlighted-line">
+
+1. Der erste Block `if (<Bedingung1>) { <Anweisung1> }`
+   - ist zwingend
+   - und zwar von `if` bis zur ersten schliessenden geschweiften Klammer.
+
+</div>
+
+<div className="code-block-green-line">
+
+2. Der nächste Block `else if (<Bedingung2>) { <Anweisung2> }`
+   - ist <u>optional</u>
+   - kann auch noch mehrfach wiederholt folgen
+   - kann nie alleine stehen _(nie ohne vorausgehender `if`-Block)_
+   - wird ausgeführt, sofern die Bedingung vom vorausgehendem `if` oder `else if` Block **false** war
+
+</div>
+
+<div className="code-block-blue-line">
+
+3. Der letzte Block `else { <Anweisung3> }`
+   - ist <u>optional</u>
+   - besitzt keine Bedingung
+   - darf <u>nur ein Mal</u> vorkommen, und zwar <u>ganz am Schluss</u>
+   - wird ausgeführt wenn kein anderer Block ausgeführt wurde
+
+</div>
+
+
+<div className="grid"><div>
+
+```java title="if: Formelles Beispiel"
+// highlight-start
+if (<Bedingung1>) {
+    <Anweisung1>
+} 
+// highlight-end
+// highlight-green-start
+else if (<Bedingung2>) { // Optionaler Block
+    <Anweisung2>
+} 
+// highlight-green-end
+// highlight-blue-start
+else {                  // Optionaler Block
+    <Anweisung3>
+}
+// highlight-blue-end
+```
+
+</div><div>
+
+```mermaid
+flowchart TD
+    C{if-Kontrollstruktur}
+    C -->|"if\n< Bedingung1 >"| D["< Anweisung1 >"]
+    C -->|"else if\n< Bedingung2 >"| E["< Anweisung2 >"]
+    C -->|else| F["< Anweisung3 >"]
+
+    style E fill:#00A17020,stroke:#00A17080,stroke-width:2px
+    style F fill:#0072B520,stroke:#0072B580,stroke-width:2px
+```
+
+</div></div>
+
+**Erläuterung**
+
+1. Wenn die `<Bedingung1>` `wahr` ist, wird <u>nur</u> die `<Anweisung1>` ausgeführt.
+2. Wenn die `<Bedingung1>` `falsch` <u>und</u> die `<Bedingung2>` `wahr` ist, wird <u>nur</u> die `<Anweisung2>` ausgeführt.
+3. Wenn die `<Bedingung1>` `falsch` <u>und</u> die `<Bedingung2>` `falsch` ist, wird <u>nur</u> die `<Anweisung3>` ausgeführt.
+
+:::tip
+
+Es wird immer nur <u>ein Anweisungs-Block</u> pro `if`-Kontrollstruktur ausgeführt!
+
+:::
+
+
+### Praktisches Beispiel
+
+<div className="grid"><div>
+
+```java showLineNumbers
+// highlight-green-next-line
+String vehicle = "velo";
+
+if ("auto".equals(vehicle)) {
+    System.out.println("Hallo Auto!");
+}
+// highlight-green-start
+else if ("velo".equals(vehicle)) {
+    System.out.println("Hallo Velo!");
+}
+// highlight-green-end
+else {
+    System.out.println("Hallo Anderer!!");
 }
 ```
 
+:::tip
 
+Die Variable `String type` könnte nun auch von der Konsole eingelesen werden und somit "dynamisch" sein. Dann macht die Kondition auch mehr Sinn!
+
+:::
+
+</div><div>
+
+```mermaid
+flowchart TD
+    A[String vehicle] -->|"velo"| B{if-Kontrollstruktur}
+    B -->|"if (auto)"| D[Hallo Auto!]
+    B -->|"else if (velo)"| E[Hallo Velo!]
+    B -->|else| F[Hallo Anderer!!]
+
+    style A fill:#00A17020,stroke:#00A17080,stroke-width:2px
+    style B fill:#00A17020,stroke:#00A17080,stroke-width:2px
+    style E fill:#00A17020,stroke:#00A17080,stroke-width:2px
+```
+
+</div></div>
+
+**Erläuterung**
+
+1. Es existiert eine Variable _vehicle_ vom Typ `String` mit dem Wert `"velo"`
+2. Die `if`-Block Bedingung _(Linie 3)_ wir ausgeführt
+   - Da es sich bei der Variable _vehicle_ um einen `String` handelt, wird mit der Methode `equals()` auf Gelchheit geprüft
+   - Die Bedingung prüft ob es sich um ein `"auto"` handelt
+   - Da der Wert von _vehicle_ `"velo"` ist, ist der `boolean` der Prüfung `false`, also falsch
+   - Der Code-Block **_(Linie 4)_ wird übersprungen**
+3. Die `else if` Bedingung _(Linie 6)_ wird nun ausgeführt
+   - Es wird geprüft ob der Wert `"velo"` ist.
+   - Da der Wert `"velo"` ist, ist der `boolean` der **Prüfung `true`, also richtig**
+   - Der **`else if`-Block _(Linie 7)_ wird ausgeführt**.
+4. Da eine Bedingung `true` war, wird der **`else`-Block _(Linie 10)_ übersprungen!**
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,19 +1,19 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-const oembed = require('@agentofuser/remark-oembed');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const oembed = require("@agentofuser/remark-oembed");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Modul 319',
-  tagline: 'Applikationen entwerfen und implementieren',
-  url: 'https://codingluke.github.io',
-  baseUrl: '/bbzbl-modul-319/',
-  onBrokenLinks: 'log',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
+  title: "Modul 319",
+  tagline: "Applikationen entwerfen und implementieren",
+  url: "https://codingluke.github.io",
+  baseUrl: "/bbzbl-modul-319/",
+  onBrokenLinks: "log",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
 
   markdown: {
     mermaid: true,
@@ -21,15 +21,15 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'codingluke', // Usually your GitHub org/user name.
-  projectName: 'bbzbl-modul-319', // Usually your repo name.
+  organizationName: "codingluke", // Usually your GitHub org/user name.
+  projectName: "bbzbl-modul-319", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'de',
-    locales: ['de'],
+    defaultLocale: "de",
+    locales: ["de"],
   },
 
   themes: [
@@ -39,38 +39,43 @@ const config = {
         hashed: true,
         language: ["de"],
         indexBlog: false,
-        indexPages: true
+        indexPages: true,
       },
     ],
     require.resolve("@docusaurus/theme-mermaid"),
   ],
-  
+
   plugins: [
-    ["devserver-config",
+    "remark-docusaurus-tabs",
+    [
+      "devserver-config",
       {
         proxy: {
           "/bbzbl-modul-319/slides": {
             target: "http://localhost:3003",
-            pathRewrite: function(/** @type {string} */ path, /** @type {any} */ _req) {
-              return path.replace("/bbzbl-modul-319/slides", '') + ".md";
-            }
-          }
-        }
-      }
-    ]
+            pathRewrite: function (
+              /** @type {string} */ path,
+              /** @type {any} */ _req
+            ) {
+              return path.replace("/bbzbl-modul-319/slides", "") + ".md";
+            },
+          },
+        },
+      },
+    ],
   ],
 
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-            // 'https://github.com/codingluke/bbzbl-modul-431/tree/main/',
+          // 'https://github.com/codingluke/bbzbl-modul-431/tree/main/',
           remarkPlugins: [oembed],
         },
         // blog: {
@@ -81,7 +86,7 @@ const config = {
         //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         // },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
@@ -91,39 +96,71 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       mermaid: {
-        theme: { light: 'neutral', dark: 'forest' },
+        theme: { light: "neutral", dark: "forest" },
       },
       navbar: {
-        title: 'Modul 431',
+        title: "Modul 431",
         logo: {
-          alt: 'BBZBL Logo',
-          src: 'img/bbzbl-logo.png',
+          alt: "BBZBL Logo",
+          src: "img/bbzbl-logo.png",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Unterlagen',
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "Unterlagen",
           },
-          { to: '/slides/', label: 'Präsentationen', position: 'left' },
+          { to: "/slides/", label: "Präsentationen", position: "left" },
           {
-            href: 'https://github.com/codingluke/bbzbl-modul-431',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/codingluke/bbzbl-modul-431",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         copyright: `Made with ❤️ in Pratteln`,
       },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['java'],
+        additionalLanguages: ["java"],
+        magicComments: [
+          // Remember to extend the default highlight class name as well!
+          {
+            className: "theme-code-block-highlighted-line",
+            line: "highlight-next-line",
+            block: { start: "highlight-start", end: "highlight-end" },
+          },
+          {
+            className: "code-block-red-line",
+            line: "highlight-red-next-line",
+            block: { start: "highlight-red-start", end: "highlight-red-end" },
+          },
+          {
+            className: "code-block-green-line",
+            line: "highlight-green-next-line",
+            block: { start: "highlight-green-start", end: "highlight-green-end" },
+          },
+          {
+            className: "code-block-yellow-line",
+            line: "highlight-yellow-next-line",
+            block: { start: "highlight-yellow-start", end: "highlight-yellow-end" },
+          },
+          {
+            className: "code-block-orange-line",
+            line: "highlight-orange-next-line",
+            block: { start: "highlight-orange-start", end: "highlight-orange-end" },
+          },
+          {
+            className: "code-block-blue-line",
+            line: "highlight-blue-next-line",
+            block: { start: "highlight-blue-start", end: "highlight-blue-end" },
+          },
+        ],
       },
     }),
 };
 module.exports = config;
-
