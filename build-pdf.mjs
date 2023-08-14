12b07a48264c70495aeebec7628eb3079a815c05
Lukas Hodel <lukas.hodel@swisscom.com>
2023-08-14 20:34:22 +0200

    Add Date and Link

--- build-pdf.mjs
+++ build-pdf.mjs
@@ -4,6 +4,7 @@ import fs from "fs";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
+const now = new Date(Date.now()) 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 global.appRoot = path.resolve(__dirname);
@@ -14,9 +15,10 @@ const getBase64ImgTag = (path) => {
   return `<img height="30px" src="data:image/png;base64, ${base64Logo}"/>`;
 };
 
+
 generatePDF({
   initialDocURLs: [
-    `http://localhost:3000/${modulConfig.repoName}/docs`,
+    `http://localhost:3000/${modulConfig.repoName}/docs/effizienter`,
   ],
   puppeteerArgs: [
     "--no-sandbox",
@@ -46,24 +48,6 @@ generatePDF({
     "iframe",
   ],
   paginationSelector: "a.pagination-nav__link.pagination-nav__link--next",
-  footerTemplate: `
-    <style>
-      .footer {
-        height: 15px; 
-        width: 100%; 
-        display: flex; 
-        justify-content: space-between; 
-        align-items: flex-end;
-        font-size: 8px; 
-        margin: 10px 55px;
-        font-family: system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont;
-        border-top: 1px solid rgba(0,0,0,0.05);
-      }
-    </style>
-    <div class="footer">
-      <div></div>
-      <div><span class="pageNumber"></span> / <span class="totalPages"></span></div>
-    </div> `,
   headerTemplate: `
     <style>
       .header {
@@ -85,4 +69,23 @@ generatePDF({
       } - ${modulConfig.tagline}</div>
       ${getBase64ImgTag("/static/img/bbzbl-logo.png")}
     </div>`,
+  footerTemplate: `
+    <style>
+      .footer {
+        height: 15px; 
+        width: 100%; 
+        display: flex; 
+        justify-content: space-between; 
+        align-items: flex-end;
+        font-size: 8px; 
+        margin: 10px 55px;
+        font-family: system-ui, -apple-system, Segoe UI, Roboto, Ubuntu, Cantarell, Noto Sans, sans-serif, BlinkMacSystemFont;
+        border-top: 1px solid rgba(0,0,0,0.05);
+      }
+    </style>
+    <div class="footer">
+      <div>${now.toLocaleDateString('de-DE', {})}</div>
+      <div>${modulConfig.url}/${modulConfig.repoName}</div>
+      <div><span class="pageNumber"></span> / <span class="totalPages"></span></div>
+    </div> `,
 });
